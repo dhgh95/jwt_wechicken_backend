0b33349818fe8d0f0e9dd7f5482a1444236ebd34
dhgh95 <ghrms2525@naver.com>
2020-08-22 00:49:19 +0900

    Add: 회원가입 로그인 만드는중

---
+++ controllers/auth.js
@@ -0,0 +1,8 @@
+const bcrypt = require("bcryptjs");
+const { errorGenerator } = require("../utils");
+
+const signUp = (req, res, next) => {
+  const user = req.body;
+};
+
+module.exports = { signUp };
