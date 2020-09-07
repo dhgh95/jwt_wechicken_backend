1c053ce9eddd497f7402efd2e077b09795b95bfc
dhgh95 <ghrms2525@naver.com>
2020-09-07 18:03:52 +0900

    Refactor: 반복되는 로직 service 분리

--- routes/posts.js
+++ routes/posts.js
@@ -4,14 +4,14 @@ const router = express.Router();
 const {
   getPostsLike,
   getPostsBookMarks,
-  postPostLike,
-  postPostBookMark,
+  isPostLike,
+  isPostBookMark,
 } = require("../controllers/posts");
 const { isAuth } = require("../middlewares");
 
 router.get("/likes", isAuth, getPostsLike);
 router.get("/bookmarks", isAuth, getPostsBookMarks);
-router.post("/likes/:blog_id", isAuth, postPostLike);
-router.post("/bookmarks/:blog_id", isAuth, postPostBookMark);
+router.post("/likes/:blog_id", isAuth, isPostLike);
+router.post("/bookmarks/:blog_id", isAuth, isPostBookMark);
 
 module.exports = router;
