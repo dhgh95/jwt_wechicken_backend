d16cdf6a15eea766bcb301c406d03d2902b3b6c2
dhgh95 <ghrms2525@naver.com>
2020-09-16 16:01:16 +0900

    Add: 카드 추가하기 API

--- scraper/utils/mediumChangeDateFormat.js
+++ scraper/utils/mediumChangeDateFormat.js
@@ -4,6 +4,8 @@ module.exports = function (originalDate) {
   if (originalDate) {
     const [month, day, year] = originalDate.split(" ");
     const currentYear = new Date().getFullYear();
-    return `${year ? year : currentYear}.${months[month]}.${day}`;
+    return `${year ? year : currentYear}.${months[month]}.${
+      day.length === 1 ? `0${day}` : day
+    }`;
   }
 };
